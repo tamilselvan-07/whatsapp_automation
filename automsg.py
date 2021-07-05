3359401c87b50fe5c7c407eabb8b3661a1bbc316
Tamilselvan <58145135+tamilselvan-07@users.noreply.github.com>
2021-07-05 10:45:36 +0530

    Create automsg.py

---
+++ automsg.py
@@ -0,0 +1,6 @@
+#pip install pywhatkit
+
+import pywhatkit as pw
+pw.sendwhatmsg("+91**********","Your message",13,59)
+
+#This module follows the 24 hrs time format, hence the time 13:59 is 01:59 PM.
